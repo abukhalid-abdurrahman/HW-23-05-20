81d21887c825ecb273a43ad9fc785205abc5dc2a
faha-berdiev <mafysoftware@gmail.com>
2020-05-24 16:55:18 +0300

    Add project files

---
+++ ModelRepository.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace Practics
+{
+    class ModelRepository<T>
+    {
+        private string connectionString = "Data Source=localhost;Initial catalog=Faridun;Integrated Security=True";
+        private string tableName;
+        public ModelRepository(string tableName)
+        {
+            this.tableName = tableName;
+        }
+        public List<T> Read()
+        {
+            List<T> model = new List<T>();
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                string sqlQuery = $"select * from {tableName}";
+                model = db.Query<T>(sqlQuery).ToList();
+            }
+            return model;
+        }
+        public int Create(T model, string columns, string values)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = $"insert into {tableName} ({columns}) values({values}); select cast(SCOPE_IDENTITY() as int)";
+                int newPersonId = db.Query<int>(sqlQuery, model).FirstOrDefault();
+                return newPersonId;
+            }
+        }
+    }
+}
---
+++ Models/PersonViewModel.cs
@@ -0,0 +1,10 @@
+namespace Practics.Models
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+    }
+}
